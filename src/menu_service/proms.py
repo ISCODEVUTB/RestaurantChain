3950d4cdfc14aacb62c93afd8252f42e7eb1f80a
Oscar Castillo <ocp529@gmail.com>
2025-04-08 11:28:28 -0500

    promotion crud service

--- src/menu_service/proms.py
+++ src/menu_service/proms.py
@@ -9,8 +9,8 @@ db = client["Restaurant"]
 menu = db["Menu"]
 
 
-def create_product():
-    name = input("Nombre del producto: ")
+def create_promo():
+    name = input("Nombre de la promocion: ")
     desc = input("Descripción: ")
     price = float(input("Precio: "))
     if price < 1000:
@@ -18,23 +18,25 @@ def create_product():
     else:
         points = price / 1000
     p_price = input("Valor en puntos: ")
-    product = {"Name": name,
-               "Category": "Product",
-               "Description": desc,
-               "Price": price,
-               "Points": points,
-               "Points Price": p_price,
-               "Aviability": "Si"}
-    menu.insert_one(product)
-    print("Producto agregado.")
+    promo = {
+        "Name": name,
+        "Category": "Promotion",
+        "Description": desc,
+        "Price": price,
+        "Points": points,
+        "Points Price": p_price,
+        "Aviability": "Si"
+    }
+    menu.insert_one(promo)
+    print("Promocion agregada.")
 
 
-def update_product():
-    edp = "Producto editado."
-    name = input("Nombre del producto a editar: ")
-    product = menu.find_one({"Name": name})
-    if not product:
-        print("Producto Inexistente.")
+def update_promo():
+    edpr = "Promocion editada."
+    name = input("Nombre de la promocion a editar: ")
+    promo = menu.find_one({"Name": name})
+    if not promo:
+        print("Promocion Inexistente.")
         return
     op = 0
     while op not in ["1", "2", "3", "4", "5", "6"]:
@@ -51,13 +53,13 @@ def update_product():
         if op == "1":
             new_name = input("Nuevo nombre: ")
             menu.update_one({"Name": name}, {"$set": {"Name": new_name}})
-            print(edp)
+            print(edpr)
         elif op == "2":
             new_desc = input("Nueva descripción: ")
             menu.update_one(
                 {"Name": name}, {"$set": {"Description": new_desc}}
             )
-            print(edp)
+            print(edpr)
         elif op == "3":
             new_price = float(input("Nuevo precio: "))
             menu.update_one({"Name": name}, {"$set": {"Price": new_price}})
@@ -66,21 +68,21 @@ def update_product():
             else:
                 points = new_price / 1000
             menu.update_one({"Name": name}, {"$set": {"Points": points}})
-            print(edp)
+            print(edpr)
         elif op == "4":
             new_points = float(input("Nuevo valor en puntos: "))
             menu.update_one(
                 {"Name": name},
                 {"$set": {"Points Price": new_points}}
             )
-            print(edp)
+            print(edpr)
         elif op == "5":
             new_availability = input("¿Disponible? (Si/No): ")
             menu.update_one(
                 {"Name": name},
                 {"$set": {"Aviability": new_availability}}
             )
-            print(edp)
+            print(edpr)
         elif op == "6":
             print("Saliendo de la edición.")
             break
@@ -88,28 +90,29 @@ def update_product():
             print("Opción inválida.")
 
 
-def delete_product():
-    name = input("Nombre del producto a eliminar: ")
+def delete_promo():
+    name = input("Nombre de la promocion a eliminar: ")
     result = menu.delete_one({"Name": name})
     if result.deleted_count > 0:
-        print("Producto eliminado.")
+        print("Promocion eliminada.")
     else:
-        print("Producto no encontrado.")
+        print("Promocion no encontrada.")
 
 
-def read_menu():
+def read_promos():
     for product in menu.find():
-        print(f"Nombre: {product['Name']}\n"
-              f"Descripcion: {product['Description']}\n"
-              f"Precio: {product['Price']}$ \n"
-              f"Puntos: {product['Points']} \n"
-              f"Valor en puntos: {product['Points Price']}\n"
-              f"Disponibilidad: {product['Aviability']}\n")
-        print("--------------------------------------------------")
+        if product["Category"] == "Promotion":
+            print(f"Nombre: {product['Name']}\n"
+                  f"Descripcion: {product['Description']}\n"
+                  f"Precio: {product['Price']}$ \n"
+                  f"Puntos: {product['Points']} \n"
+                  f"Valor en puntos: {product['Points Price']}\n"
+                  f"Disponibilidad: {product['Aviability']}\n")
+            print("--------------------------------------------------")
 
 
-def search_product():
-    name = input("Nombre del producto a buscar: ")
+def search_promo():
+    name = input("Nombre de la promocion a buscar: ")
     product = menu.find_one({"Name": name})
     if product:
         print(f"Nombre: {product['Name']}\n"
@@ -122,30 +125,30 @@ def search_product():
         print("Producto no encontrado.")
 
 
-def mostrar_menu_desarrollador():
+def show_proms_admin():
     while True:
-        print("\n--- Gestion de Menu ---")
-        print("1. Crear producto")
-        print("2. Editar producto")
-        print("3. Eliminar producto")
-        print("4. Listar menu")
-        print("5. Buscar producto")
+        print("\n--- Gestion de Promociones ---")
+        print("1. Crear promocion")
+        print("2. Editar promocion")
+        print("3. Eliminar promocion")
+        print("4. Listar promociones")
+        print("5. Buscar promocion")
         print("6. Salir")
         opcion = input("Selecciona una opcion: ")
         if opcion == "1":
-            create_product()
+            create_promo()
         elif opcion == "2":
-            update_product()
+            update_promo()
         elif opcion == "3":
-            delete_product()
+            delete_promo()
         elif opcion == "4":
-            read_menu()
+            read_promos()
         elif opcion == "5":
-            search_product()
+            search_promo()
         elif opcion == "6":
             break
         else:
             print("Opción inválida.")
 
 
-mostrar_menu_desarrollador()
+show_proms_admin()
